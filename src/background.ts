951661f60210dce1b5c4a8a8313612741d29143e
Gervasio Marchand <gmc@gmc.uy>
2018-08-26 00:25:30 -0300

    First batch of customizations!

--- src/background.ts
+++ src/background.ts
@@ -125,5 +125,122 @@ chrome.storage.sync.get(['settings'], res => {
 
         // replace the native WebSocket with the proxy
         (window as any).WebSocket = WebSocketProxy;
+
+        if (settings.hangout_url) {
+            // need to wait until we have the objects
+            const hangoutInterval = setInterval(() => {
+                const w: any = window;
+                if (w.TS && w.TS.view && w.TS.view.submit) {
+                    clearInterval(hangoutInterval);
+                } else {
+                    return;
+                }
+
+                var originalSubmit = w.TS.view.submit;
+                var re = /@([^@>]+)>/g
+
+                w.TS.view.submit = function () {
+                    var message = w.TS.utility.contenteditable.value(w.TS.client.ui.$msg_input);
+                    var lMessage = message.toLowerCase();
+                    var userNames = [w.TS.model.user.profile.display_name];
+                    var match;
+
+                    if (lMessage.indexOf("hangout ") === 0) {
+                        var name = message.substring(8);
+                        var url;
+
+                        while (match = re.exec(name)) {
+                            userNames.push(match[1]);
+                        }
+
+                        if (userNames.length > 1) {
+                            userNames = userNames.map(u => u.normalize('NFD').replace(/[\u0300-\u036f]/g, ""));
+                            userNames.sort();
+                            url = userNames.join('-');
+                        } else {
+                            // just use the text separated by hyphens
+                            url = name.replace(' ', '-');
+                        }
+
+                        url = url.toLowerCase().replace(/[^a-zA-Z0-9-]/g, "-").replace(/-+/g, "-");
+                        w.TS.utility.contenteditable.value(w.TS.client.ui.$msg_input, `hangout ${name}: ${settings.hangout_url.replace('$name$', url)}`);
+                    }
+
+                    return originalSubmit();
+                }
+            }, 500);
+        }
+
+        if (settings.threads_on_channel) {
+            // always reply to the channel... TODO: Don't use DOMNodeInserted
+            document.addEventListener('DOMNodeInserted', function (e) {
+                const cl = (e.target as any).classList;
+                if (cl && cl.contains('reply_container_info')) {
+                    [...document.getElementsByClassName('reply_broadcast_toggle')].forEach(el => {
+                        el.setAttribute('checked', 'true');
+                    });
+                }
+            });
+        }
+
+        let css = '';
+        if (settings.hide_status_emoji) {
+            css += `
+.c-message__content_header .emoji-outer.emoji-sizer {
+    display: none !important;
+}`;
+        }
+        if(settings.reactions_on_the_right){
+            css += `
+.c-reaction_bar {
+    position: absolute;
+    bottom: 5px;
+    right: 1.25rem;
+}
+.c-message__actions--menu-showing, .c-message__actions {
+    top: unset !important;
+    bottom: 28px;
+}
+@media screen and (max-width: 1100px) {
+    .c-reaction_bar {
+        display: none;
+    }
+}
+.c-reaction_add, .c-reaction_add:hover {
+    display: none !important;
+}
+`;
+        }
+
+        if (css) {
+            var sheet = document.createElement('style');
+            sheet.type = 'text/css';
+            (window as any).customSheet = sheet;
+            (document.head || document.getElementsByTagName('head')[0]).appendChild(sheet);
+            sheet.appendChild(document.createTextNode(css));
+        }
+
+        // I had to
+        var interval = setInterval(() => {
+            var targetNode = document.querySelector(".messages_header");
+            if (targetNode) {
+                clearInterval(interval);
+            } else {
+                return;
+            }
+            var observerOptions = {
+                childList: true,
+                attributes: true,
+                subtree: true
+            }
+
+            var observer = new MutationObserver((e, observer) => {
+                var text = document.getElementById('channel_topic_text');
+                if (text && text.innerText === 'Not the Slack complaint room.') {
+                    text.innerHTML = '<strike>Not</strike> the Slack <strike>complaint</strike> <span style="color: red; font-weight: bold">modding</span> room.';
+                }
+            });
+            observer.observe(targetNode, observerOptions);
+        }, 200);
     }, data);
 });
--- src/options.ts
+++ src/options.ts
@@ -4,34 +4,32 @@ form.addEventListener('submit', e => {
 
     var formData = new FormData(form as HTMLFormElement);
     var object: any = {};
-    formData.forEach(function(value, key){
+    formData.forEach(function (value, key) {
         object[key] = value;
     });
-    object.only_my_reactions = !!object.only_my_reactions;
-    object.hide_gdrive_preview = !!object.hide_gdrive_preview;
 
     var json = JSON.stringify(object);
 
     chrome.storage.sync.set({
         'settings': json
-    }, ()=>window.close());
+    }, () => window.close());
 });
 
-setTimeout(()=>{
+setTimeout(() => {
     chrome.storage.sync.get(['settings'], res => {
         const settings = JSON.parse(res.settings || '{}');
 
         let e: any = document.getElementById('hidden_ids');
-        e.value = settings.hidden_ids;
+        e.value = settings.hidden_ids || "";
 
-        e = document.getElementById('only_my_reactions');
-        if(settings.only_my_reactions){
-            e.setAttribute('checked', true);
-        }
+        e = document.getElementById('hangout_url');
+        e.value = settings.hangout_url || "";
 
-        e = document.getElementById('hide_gdrive_preview');
-        if(settings.hide_gdrive_preview){
-            e.setAttribute('checked', true);
-        }
+        ['only_my_reactions', 'hide_gdrive_preview', 'threads_on_channel',
+         'hide_status_emoji', 'reactions_on_the_right'].forEach(f=>{
+            if(settings[f]) {
+                document.getElementById(f).setAttribute('checked', 'true');
+            }
+        })
     })
 }, 100)
