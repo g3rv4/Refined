46ba9df4ae7cdf3006fe2aaeb42c9ef05a222222
Gervasio Marchand <gmc@gmc.uy>
2018-09-14 11:06:21 -0300

    Switch to grunt!
    There was really no reason to use webpack...

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        ts: {
+            prod: {
+                tsconfig: './tsconfig.json'
+            },
+            dev: {
+                tsconfig: './tsconfig.json',
+                watch: 'src/'
+            }
+        },
+        copy: {
+            static: {
+                files: [
+                    { expand: true, src: ['**'], cwd: 'src/static/', dest: 'dist-ff/' },
+                ],
+            },
+            chrome: {
+                files: [
+                    { expand: true, src: ['**'], cwd: 'dist-ff/', dest: 'dist-chrome/' },
+                ]
+            }
+        },
+        clean: {
+            all: {
+                src: ['dist-ff', 'dist-chrome', 'zip']
+            }
+        },
+        watch: {
+            static: {
+                files: 'src/static/**/*',
+                tasks: ['copy:static']
+            },
+            ts: {
+                files: 'dist-ff/**/*',
+                tasks: ['copy:chrome', 'fix-chrome']
+            }
+        },
+        concurrent: {
+            dev: {
+                tasks: ['ts:dev', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+        compress: {
+            ff: {
+                options: {
+                    archive: 'zip/firefox.zip'
+                },
+                files: [
+                    { src: ['**/*'], dest: '/', cwd: 'dist-ff/', expand: true },
+                ]
+            },
+            chrome: {
+                options: {
+                    archive: 'zip/chrome.zip'
+                },
+                files: [
+                    { src: ['**/*'], dest: '/', cwd: 'dist-chrome/', expand: true },
+                ]
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+
+    grunt.registerTask("build", ["clean", "ts:prod", "copy", "fix-chrome", "compress"]);
+    grunt.registerTask("default", ['clean', 'copy', 'concurrent']);
+
+    grunt.registerTask('fix-chrome', "Fix Chrome's manifest", function () {
+        let manifest = grunt.file.readJSON("dist-chrome/manifest.json");
+        delete manifest.applications;
+        grunt.file.write("dist-chrome/manifest.json", JSON.stringify(manifest, null, 2));
+    });
+};
--- dist-chrome/options.js
+++ dist-chrome/options.js
@@ -1,49 +1,39 @@
-const reloadSlackTabs = (callback?: () => void) => {
+const reloadSlackTabs = (callback) => {
     chrome.tabs.query({ url: 'https://*.slack.com/*' }, tabs => {
         const tabsToReload = tabs.filter(t => t.url.match(/^https:\/\/[^\.]+\.slack\.com/));
         let tabsRemaining = tabsToReload.length;
-
         tabsToReload.forEach(t => chrome.tabs.reload(t.id, null, () => {
             tabsRemaining--;
             if (tabsRemaining == 0 && callback) {
                 callback();
             }
-        }))
+        }));
     });
 };
-
 const form = document.getElementById('the-form');
 form.addEventListener('submit', e => {
     e.preventDefault();
-
-    var formData = new FormData(form as HTMLFormElement);
-    var object: any = {};
+    var formData = new FormData(form);
+    var object = {};
     formData.forEach(function (value, key) {
         object[key] = value;
     });
-
     var json = JSON.stringify(object);
-
     chrome.storage.sync.set({
         'settings': json
     }, () => reloadSlackTabs(closePopup));
 });
-
 function closePopup() {
     window.close();
 }
-
 const uninstall = document.getElementById('uninstall');
 uninstall.addEventListener('click', e => {
     e.preventDefault();
-
     chrome.management.uninstallSelf({ showConfirmDialog: true });
-})
-
+});
 const accept = document.getElementById('accept');
 accept.addEventListener('click', e => {
     e.preventDefault();
-
     chrome.storage.sync.set({
         'acceptedRisks': new Date()
     }, () => {
@@ -54,40 +44,36 @@ accept.addEventListener('click', e => {
         reloadSlackTabs();
         alert('here');
     });
-})
-
-
+});
 setTimeout(() => {
     chrome.storage.sync.get(['acceptedRisks', 'settings'], res => {
         const html = document.querySelector('html');
         if (res.acceptedRisks) {
             html.classList.add('accepted');
-        } else {
+        }
+        else {
             html.classList.add('not-accepted');
         }
         html.classList.remove('loading');
-
         const settings = JSON.parse(res.settings || '{}');
-
-        let e: any = document.getElementById('hidden_ids');
+        let e = document.getElementById('hidden_ids');
         e.value = settings.hidden_ids || "";
-
         e = document.getElementById('hangout_url');
         e.value = settings.hangout_url || "";
-
         ['only_my_reactions', 'hide_gdrive_preview', 'threads_on_channel',
             'hide_status_emoji', 'reactions_on_the_right', 'hide_url_previews',
             'unread_on_title'].forEach(f => {
-                if (settings[f]) {
-                    document.getElementById(f).setAttribute('checked', 'true');
-                }
-            })
-    })
-}, 100)
-
+            if (settings[f]) {
+                document.getElementById(f).setAttribute('checked', 'true');
+            }
+        });
+    });
+}, 100);
 const html = document.querySelector('html');
 if (document.URL.indexOf("fullpage=1") !== -1) {
     html.classList.add('full-page');
-} else {
+}
+else {
     html.classList.add('popup');
 }
+//# sourceMappingURL=options.js.map
