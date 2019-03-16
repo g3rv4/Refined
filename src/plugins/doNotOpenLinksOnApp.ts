a79ce10b873b5aa0edf5f977e38c32e4cdf45445
Gervasio Marchand <github@gervas.io>
2019-03-16 20:03:12 -0300

    Fix the do not open links on app plugin
    closes #29

--- src/plugins/doNotOpenLinksOnApp.ts
+++ src/plugins/doNotOpenLinksOnApp.ts
@@ -1,19 +1,27 @@
 import BasePlugin from "./basePlugin";
 
 export default class DoNotOpenLinksOnApp extends BasePlugin {
-    public async init() {
-        const old = await this.getElement(() => {
-            const w = window as any;
-            return w.TS && w.TS.magic_login;
-        });
+    public constructor(name: string, settings: any) {
+        super(name, settings);
 
-        const w = window as any;
-        w.TS.magic_login.execute = (obj: any) => {
-            if (obj.browser_uri) {
-                window.location.href = obj.browser_uri;
-            } else {
-                return old(obj);
+        let overriden = false;
+
+        // this can't be done on init() because this function is called in an anonymous function in the document
+        // also, to ensure w.TS.magic_logic.execute is injected right before it's used, I'm doing this trick...
+        // overriding it when hasFocus is called (they're calling hasFocus right before using it)
+        const originalHasFocus = document.hasFocus.bind(document);
+        document.hasFocus = () => {
+            if (!overriden) {
+                const w = window as any;
+                if (w.TS && w.TS.magic_login && w.TS.magic_login.execute) {
+                    w.TS.magic_login.execute = (obj: any) => {
+                        window.location.href = obj.browser_uri;
+                    };
+
+                    overriden = true;
+                }
             }
+            return originalHasFocus();
         };
     }
 }
